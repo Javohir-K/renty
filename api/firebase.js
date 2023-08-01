ae1f70a5a7eb68333e81cba2051735937e7c2891
JAVOHIR KOMILJONOV <61010500+Javohir-K@users.noreply.github.com>
2023-08-02 01:41:03 +0500

    Update firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -3,12 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyA9sqT_CrgcX4vlD2W9bhyiTbvXrSQrIvQ",
-  authDomain: "renty-food.firebaseapp.com",
-  projectId: "renty-food",
-  storageBucket: "renty-food.appspot.com",
-  messagingSenderId: "55923691966",
-  appId: "1:55923691966:web:c267d2d63454e5f22969ef",
+// Your firebase api config code here
 };
 
 // Initialize Firebase
